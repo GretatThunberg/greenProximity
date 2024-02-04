bd7add331d9ccdb779d0aa9b05176d98a8f0ff85
yassine128 <yassineseddaoui@gmail.com>
2024-02-03 21:49:16 -0500

    automatic update of text

--- client/src/app/pages/aifeedback/aifeedback.component.ts
+++ client/src/app/pages/aifeedback/aifeedback.component.ts
@@ -1,7 +1,9 @@
 /* eslint-disable */
 
 import { Component, OnInit } from '@angular/core';
+import { SharedServiceService } from '@app/services/shared-service.service';
 import axios from 'axios';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-aifeedback',
@@ -15,8 +17,11 @@ export class AifeedbackComponent implements OnInit {
   data: { model: string; messages: { role: string; content: string; }[]; } | undefined;
   headers: { 'Content-Type': string; Authorization: string; } | undefined;
 
-  public prompt: string | undefined;
   public info: string | undefined; 
+
+  subscription: Subscription; 
+
+  constructor(private sharedService: SharedServiceService) { }
 
   ngOnInit(): void {
     this.apiKey = 'shuttle-qptdtyscfedarezef2xh';
@@ -27,14 +32,19 @@ export class AifeedbackComponent implements OnInit {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${this.apiKey}`
     };
+
+      this.subscription = this.sharedService.currentData.subscribe(data => {
+        const prompt = "Given this: " + JSON.stringify(data) + "Describe to me in words what you think about what is around me in a 15 minutes walk radius and how it could be improved (Only in a bullet point)";
+        this.generateAnswer(prompt);
+      }      
+    );
   }
 
-
-  async getDataFromOpenAPI() {
+  async generateAnswer(prompt: string) {
     this.data = {
       model: this.model ?? '',
       messages: [
-        { role: 'user', content: this.prompt ?? ''}
+        { role: 'user', content: prompt ?? ''}
       ]
     };
 
