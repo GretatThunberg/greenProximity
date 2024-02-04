57268c38c72b03adc7a45b33d3d1cf27044524a4
yassine128 <yassineseddaoui@gmail.com>
2024-02-03 21:12:08 -0500

    interface

---
+++ client/src/app/interfaces/count.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export interface Count {
+    school: number;
+    restaurant: number,
+    groceryStore: number;
+    park: number;
+    drugStore: number;
+    gym: number;
+    hospital: number;
+    bus: number;
+    metro: number;
+}
--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,5 +1,8 @@
+/* eslint-disable */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AifeedbackComponent } from '@app/pages/aifeedback/aifeedback.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -9,6 +12,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'ai', component: AifeedbackComponent},
     { path: '**', redirectTo: '/home' },
 ];
 
