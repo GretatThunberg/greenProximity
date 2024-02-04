b359840c5f26e4e0d482f327f97709b6521b1218
yassine128 <yassineseddaoui@gmail.com>
2024-02-03 20:45:20 -0500

    feature/ai feedback

---
+++ client/src/app/pages/aifeedback/aifeedback.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aifeedback',
+  templateUrl: './aifeedback.component.html',
+  styleUrls: ['./aifeedback.component.scss']
+})
+export class AifeedbackComponent {
+
+}
